8558f0dc473c2922a3e7d916277214439570f4ad
Guilherme Sanches <guitdsanches@gmail.com>
2024-04-16 11:19:43 -0300

    feat: adding sliver app bar and news list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:inews/components/main_infos.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -23,53 +23,109 @@ class _HomePageState extends State<HomePage> {
     double imageProportion = 0.55;
 
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: Opacity(
-              opacity: 1.0,
-              child: Image.asset(
-                'assets/images/news-image-background.jpg',
-                height: size.height * imageProportion,
-                fit: BoxFit.cover,
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: size.height * imageProportion,
+              floating: false,
+              pinned: true,
+              backgroundColor: Colors.blueGrey[200],
+              title: const Text(
+                'inews',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
               ),
-            ),
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: Opacity(
-              opacity: 0.2,
-              child: Container(
-                height: size.height * imageProportion,
-                color: Colors.black,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Opacity(
+                      opacity: 1.0,
+                      child: Image.asset(
+                        'assets/images/news-image-background.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned.fill(
+                      child: Container(
+                        color: Colors.black.withOpacity(0.2),
+                      ),
+                    ),
+                    Positioned.fill(
+                        top: size.height * 0.37, child: const MainInfos()),
+                  ],
+                ),
               ),
-            ),
-          ),
-          AppBar(
-            title: const Text('inews'),
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.transparent,
-            actions: [
-              IconButton(
+              actions: [
+                IconButton(
                   onPressed: () => print(''),
-                  icon: const Icon(Icons.notifications_none_outlined)),
-            ],
-          ),
-          Column(
-            children: [
-              SizedBox(
-                height: size.height * 0.35,
+                  icon: const Icon(
+                    Icons.notifications_none_outlined,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ];
+        },
+        body: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 20, top: 10, bottom: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Latest News',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                  ),
+                ],
               ),
-              const MainInfos(),
-            ],
-          )
-        ],
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Row(
+                children: newsTypes.map((type) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: Text(
+                      type,
+                      style:
+                          const TextStyle(fontSize: 14, color: Colors.black87),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ListView.separated(
+                  itemBuilder: (context, index) => const Text('test'),
+                  separatorBuilder: (context, index) => const SizedBox(
+                    height: 20,
+                  ),
+                  itemCount: 50,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+List<String> newsTypes = [
+  'All',
+  'Football',
+  'Health',
+  'Politics',
+  'Food',
+  'House',
+  'Travel'
+];
